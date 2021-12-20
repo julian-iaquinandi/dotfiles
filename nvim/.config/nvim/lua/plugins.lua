10abe63a3a3751384ff3bc27bf9b730769606a95
kensleDev <kensledev@gmail.com>
2021-12-20 17:37:03 +0000

    Building python

---
+++ nvim/.config/nvim/lua/plugins.lua
@@ -0,0 +1,79 @@
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+end
+
+return require('packer').startup(function()
+  use 'wbthomason/packer.nvim'
+  use 'folke/tokyonight.nvim'
+  use 'JoosepAlviste/nvim-ts-context-commentstring'
+  use 'tpope/vim-commentary'
+  use 'ThePrimeagen/git-worktree.nvim'
+  use 'mhinz/vim-startify'
+  use {
+    'neoclide/coc.nvim', 
+    branch = 'release',
+    run = ':CocInstall coc-tsserver'
+  }
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate',
+    config = function()
+      require'nvim-treesitter.configs'.setup {
+        ensure_installed = "maintained",
+        context_commentstring = {
+          enable = true
+        },
+        highlight = {
+          enable = true
+        },
+        indent = {
+          enable = true
+        }
+      }
+    end
+  }
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = {'kyazdani42/nvim-web-devicons', opt = true},
+    config = function()
+      require('lualine').setup()
+    end
+  }
+  use {
+    'nvim-telescope/telescope.nvim',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+  use {
+    "blackCauldron7/surround.nvim",
+    config = function()
+      require"surround".setup {mappings_style = "surround"}
+    end
+  }
+  use {
+    "folke/which-key.nvim",
+    config = function()
+      require("which-key").setup {
+    }
+    end
+  }
+  use {
+    'phaazon/hop.nvim',
+    branch = 'v1', -- optional but strongly recommended
+    config = function()
+      -- you can configure Hop the way you like here; see :h hop-config
+      require'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
+    end
+  }
+  use {
+    'lewis6991/gitsigns.nvim',
+    requires = {
+      'nvim-lua/plenary.nvim'
+    },
+    config = function()
+      require('gitsigns').setup()
+    end
+  }
+
+end)
