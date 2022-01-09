5a30c07d3e2d0d6aed7161cc1b9028b80d20ba04
kensleDev <kensledev@gmail.com>
2022-01-09 12:31:37 +0000

    Updated nvim.test, now opens files at line

--- scripts/tmux-test.sh
+++ scripts/tmux-test.sh
@@ -2,13 +2,18 @@
 
 FIND_LAST_VITE_ERROR() {
 
+  # Get file path 
+  tmux copy-mode
   tmux send -X search-backward 'File: '
   tmux send-keys w
   tmux send-keys v
   tmux send-keys $
-  copy-pipe-and-cancel 'xargs -I{} nvr --remote {}' 
+  tmux send-keys M-f y
+  FILE=$(clip)
 
-  
+
+  # Get file number
+  tmux copy-mode
   tmux send -X search-backward 'File: '
   tmux send-keys j
   tmux send-keys j
@@ -18,28 +23,36 @@ FIND_LAST_VITE_ERROR() {
   tmux send-keys l
   tmux send-keys l
   tmux send-keys M-f y
-  NUM=$(tmux)
+  NUM=$(clip)
+  
+  OPEN_FILE_AT_LINE $FILE:$NUM
+
+  tmux select-pane -t 0
+
 }
 
-COPY_ERRORS() {
-  tmux send -X search-backward 'problems ('
-  tmux send-keys n
-  tmux send -X search-forward 'ERROR in'
-  tmux send-keys j
-  tmux send-keys V
-  tmux send -X search-forward 'problems ('
-  tmux send-keys kk
-  tmux send-keys y
+OPEN_FILE_AT_LINE() {
+  # Open file at line
+  tmux split -v
+  # tmux send-keys 'cat '$FILE
+  result=$(echo "$1" | tr '\r' ' ')
+  fileName=$(echo $result | cut -d' ' -f1)
+
+  nvr --remote +$2 $fileName
+  tmux kill-pane
 }
 
-OPEN_FILE_WITH_VIM() {
-  nvr --remote $1
-}
+# COPY_ERRORS() {
+#   tmux send -X search-backward 'problems ('
+#   tmux send-keys n
+#   tmux send -X search-forward 'ERROR in'
+#   tmux send-keys j
+#   tmux send-keys V
+#   tmux send -X search-forward 'problems ('
+#   tmux send-keys kk
+#   tmux send-keys y
+# }
 
-COPY_ERRORS
+FIND_LAST_VITE_ERROR
 
-# OPEN_FILE_WITH_VIM file1
-
-# tmux paste-buffer | clip.exe
-#
 
