e167443e2d12f3f20f0be3a96e53eba3fd0cba7c
kensleDev <kensledev@gmail.com>
2022-11-04 15:44:54 +0000

    Update profile.ps1

--- powershell/.config/powershell/profile.ps1
+++ powershell/.config/powershell/profile.ps1
@@ -78,31 +78,3 @@ function which ($command) {
   Get-Command -Name $command -ErrorAction SilentlyContinue |
     Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
 }
-
-function installFontsInThisDir() {
-echo "Install fonts"
-$fonts = (New-Object -ComObject Shell.Application).Namespace(0x14)
-foreach ($file in gci *.ttf)
-{
-    $fileName = $file.Name
-    if (-not(Test-Path -Path "C:\Windows\fonts\$fileName" )) {
-        echo $fileName
-        dir $file | %{ $fonts.CopyHere($_.fullname) }
-    }
-}
-cp *.ttf c:\windows\fonts\
-}
-
-
-function nerdFontInstall() {
-# Install nerd font from https://www.nerdfonts.com/font-downloads
-$fontName = "AnonymousPro"
-Invoke-WebRequest -Uri "https://gist.githubusercontent.com/kensleDev/0ac20b2dff6d856404c3d332aaf50f6d/raw/f276f1cde53c1ccb5876bbe0c5affd93407c9de4/font-dir-install.ps1"  -OutFile "~\Downloads\font-dir-install.ps1"
-Invoke-WebRequest -Uri "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/$fontName.zip"  -OutFile "~\Downloads\$fontName.zip"
-cd ~\Downloads
-Expand-Archive "~\Downloads\$fontName.zip"
-cd "~\Downloads\$fontName"
-. ~\Downloads\font-dir-install.ps1
-cd ~\Downloads
-rm -r "~\Downloads\$fontName"
-}
