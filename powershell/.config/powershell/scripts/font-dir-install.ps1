01e2ddbed6c48f14e4840a23eec4e4b2aeb6a17a
kensleDev <kensledev@gmail.com>
2022-11-04 15:44:26 +0000

    Create font-dir-install.ps1

---
+++ powershell/.config/powershell/scripts/font-dir-install.ps1
@@ -0,0 +1,11 @@
+echo "Install fonts"
+$fonts = (New-Object -ComObject Shell.Application).Namespace(0x14)
+foreach ($file in gci *.ttf)
+{
+    $fileName = $file.Name
+    if (-not(Test-Path -Path "C:\Windows\fonts\$fileName" )) {
+        echo $fileName
+        dir $file | %{ $fonts.CopyHere($_.fullname) }
+    }
+}
+cp *.ttf c:\windows\fonts\
