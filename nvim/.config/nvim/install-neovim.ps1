9e49a8a9c3e83438271aa953615022014f40dca9
kensleDev <kensledev@gmail.com>
2022-11-07 17:43:45 +0000

    Update install-neovim.ps1

--- nvim/.config/nvim/install-neovim.ps1
+++ nvim/.config/nvim/install-neovim.ps1
@@ -1,13 +1,14 @@
 if($IsMac) {
-  brew install neovim pyenv ripgrep
+  brew install neovim pyenv ripgrep wget gzip
   pyenv install 3.9.2 
   nvm install lts
   nvm use lts
 } else {
   cp -r ~/dotfiles/nvim/.config/* ~/AppData/Local/
-  choco install -y neovim ripgrep llvm
+  choco install -y neovim ripgrep llvm wget gzip
   choco install -y python3 --pre
   nvm install lts
   nvm use lts
+  npm install -g neovim
 }
 
