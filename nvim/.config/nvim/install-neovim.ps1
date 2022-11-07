32f1d130aeeaa4340e1cee40bf7bddbba6cf130d
kensleDev <kensledev@gmail.com>
2022-11-07 17:22:27 +0000

    Update install-neovim.ps1

--- nvim/.config/nvim/install-neovim.ps1
+++ nvim/.config/nvim/install-neovim.ps1
@@ -5,7 +5,7 @@ if($IsMac) {
   nvm use lts
 } else {
   cp -r ~/dotfiles/nvim/.config/* ~/AppData/Local/
-  choco install -y neovim ripgrep
+  choco install -y neovim ripgrep llvm
   choco install -y python3 --pre
   nvm install lts
   nvm use lts
