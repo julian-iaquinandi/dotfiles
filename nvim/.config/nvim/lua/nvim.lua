794e57182efb86659c19e55b6cd16dec46c78c3d
kensledev <kensledev@gmail.com>
2022-12-12 19:05:24 +0000

    updated ansible

--- nvim/.config/nvim/lua/nvim.lua
+++ nvim/.config/nvim/lua/nvim.lua
@@ -5,25 +5,16 @@ require("user.whichkey")
 require("user.colors")
 require("user.telescope")
 
-<<<<<<< HEAD
 require("user.lspn.mason")
 require("user.lspn.lspconfig")
-require("user.lspn.lspsaga")
-require("user.lspn.null-ls")
-=======
-require("user.lsp.mason")
-require("user.lsp.lspconfig")
 require("user.lsp.lspsaga")
-require("user.lsp.null-ls")
->>>>>>> main
+require("user.lspn.null-ls")
 
 require("user.nvim-cmp")
 require("user.trouble")
 require("user.luasnip")
-<<<<<<< HEAD
 require("user.copilot")
-=======
->>>>>>> main
+
 -- require 'user.lsp'
 -- require 'user.lspn'
 -- require 'user.symbol-outline'
