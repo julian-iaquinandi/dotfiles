3157c350673382ad7e6c95140d156ebb2d49335e
julianiaquinandi <kensledev@gmail.com>
2023-02-25 23:36:20 +0000

    Completion working as before

--- nvim/.config/nvim/lua/plugins/completion.lua
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -1,92 +1,101 @@
 return {
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = { 
-      "hrsh7th/cmp-emoji",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-cmdline",
-      "hrsh7th/cmp-nvim-lua",
-      "hrsh7th/cmp-nvim-lsp",
-      "saadparwaiz1/cmp_luasnip",
-      "L3MON4D3/LuaSnip",
-      "rafamadriz/friendly-snippets"
-    },
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"onsails/lspkind.nvim",
+			"hrsh7th/cmp-emoji",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-cmdline",
+			"hrsh7th/cmp-nvim-lua",
+			"hrsh7th/cmp-nvim-lsp",
+			"saadparwaiz1/cmp_luasnip",
+			"L3MON4D3/LuaSnip",
+			"rafamadriz/friendly-snippets",
+		},
 
-    opts = function(_, opts)
-      local cmp = require("cmp")
-      opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { 
-        { name = "emoji" },
-        { name = "nvim_lsp" },
-        { name = "luasnip" },
-        { name = "nvim_lsp" },
-        { name = "nvim_lua" },
-        { name = "buffer" },
-        { name = "path" },
-      }))
-    end
-  }
+		event = "VeryLazy",
+
+		opts = function()
+			-- local cmp = require("cmp")
+			local lspkind = require("lspkind")
+			local luasnip = require("luasnip")
+			local cmp = require("cmp")
+
+			return {
+				sources = {
+					{ name = "emoji" },
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" },
+					{ name = "nvim_lsp" },
+					{ name = "nvim_lua" },
+					{ name = "buffer" },
+					{ name = "path" },
+				},
+
+				snippet = {
+					expand = function(args)
+						luasnip.lsp_expand(args.body) -- For `luasnip` users.
+					end,
+				},
+
+				fomatting = {
+					fields = { "kind", "abbr", "menu" },
+					format = lspkind.cmp_format({
+						maxwidth = 50,
+						ellipsis_char = "...",
+					}),
+				},
+
+				confirm_opts = {
+					behavior = cmp.ConfirmBehavior.Replace,
+					select = false,
+				},
+
+				mapping = {
+					-- ["<C-k>"] = cmp.mapping.select_prev_item(),
+					-- ["<C-l>"] = cmp.mapping.select_next_item(),
+					-- ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
+					-- ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
+					["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+					["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+					["<C-e>"] = cmp.mapping({
+						i = cmp.mapping.abort(),
+						c = cmp.mapping.close(),
+					}),
+					-- Accept currently selected item. If none selected, `select` first item.
+					-- Set `select` to `false` to only confirm explicitly selected items.
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						elseif luasnip.expandable() then
+							luasnip.expand()
+						elseif luasnip.expand_or_jumpable() then
+							luasnip.expand_or_jump()
+						elseif check_backspace() then
+							fallback()
+						else
+							fallback()
+						end
+					end, {
+						"i",
+						"s",
+					}),
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						elseif luasnip.jumpable(-1) then
+							luasnip.jump(-1)
+						else
+							fallback()
+						end
+					end, {
+						"i",
+						"s",
+					}),
+				},
+			}
+		end,
+	},
 }
-
-      -- opts.snippet = {
-      --   expand = function(args)
-      --     luasnip.lsp_expand(args.body) -- For `luasnip` users.
-      --   end,
-      -- },
-
-      -- opts.fomatting = {
-      --   fields = { "kind", "abbr", "menu" },
-      --   format = lspkind.cmp_format({
-      --     maxwidth = 50,
-      --     ellipsis_char = "...",
-      --   })
-      -- },
-
-      -- opts.confirm_opts = {
-      --   behavior = cmp.ConfirmBehavior.Replace,
-      --   select = false,
-      -- },
-
-      -- opts.mapping = {
-      --   ["<C-k>"] = cmp.mapping.select_prev_item(),
-      --   ["<C-l>"] = cmp.mapping.select_next_item(),
-      --   ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
-      --   ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
-      --   ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-      --   ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-      --   ["<C-e>"] = cmp.mapping {
-      --     i = cmp.mapping.abort(),
-      --     c = cmp.mapping.close(),
-      --   },
-      --   -- Accept currently selected item. If none selected, `select` first item.
-      --   -- Set `select` to `false` to only confirm explicitly selected items.
-      --   ["<CR>"] = cmp.mapping.confirm { select = true },
-      --   ["<Tab>"] = cmp.mapping(function(fallback)
-      --     if cmp.visible() then
-      --       cmp.select_next_item()
-      --     elseif luasnip.expandable() then
-      --       luasnip.expand()
-      --     elseif luasnip.expand_or_jumpable() then
-      --       luasnip.expand_or_jump()
-      --     elseif check_backspace() then
-      --       fallback()
-      --     else
-      --       fallback()
-      --     end
-      --   end, {
-      --     "i",
-      --     "s",
-      --   }),
-      --   ["<S-Tab>"] = cmp.mapping(function(fallback)
-      --     if cmp.visible() then
-      --       cmp.select_prev_item()
-      --     elseif luasnip.jumpable(-1) then
-      --       luasnip.jump(-1)
-      --     else
-      --       fallback()
-      --     end
-      --   end, {
-      --     "i",
-      --     "s",
-      --   }),
-      -- },
--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -1,29 +1,28 @@
 return {
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      -- load the colorscheme here
-      vim.cmd([[colorscheme tokyonight]])
-    end,
-  },
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			-- load the colorscheme here
+			vim.cmd([[colorscheme tokyonight]])
+		end,
+	},
 
-  {	"ojroques/vim-oscyank", lazy = true },
+	{ "ojroques/vim-oscyank", lazy = true },
 
-  { "tpope/vim-commentary" },
+	{ "tpope/vim-commentary", event = "VeryLazy" },
 
-  { "christoomey/vim-tmux-navigator" },
+	{ "christoomey/vim-tmux-navigator" },
 
-  { 
-    "ggandor/leap.nvim",
-    event = "VeryLazy",
-    dependencies = {
-      "tpope/vim-repeat"
-    },
-    config = function()
-      require('leap').add_default_mappings()
-    end
-  }
-
+	{
+		"ggandor/leap.nvim",
+		event = "VeryLazy",
+		dependencies = {
+			"tpope/vim-repeat",
+		},
+		config = function()
+			require("leap").add_default_mappings()
+		end,
+	},
 }
