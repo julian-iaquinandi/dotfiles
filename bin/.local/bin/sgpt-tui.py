f191058771e7d63fd96ed72cebf5692ef1956e2d
kensleDev <kensledev@gmail.com>
2023-06-05 22:12:19 +0000

    Added chat picker

--- bin/.local/bin/sgpt-tui.py
+++ bin/.local/bin/sgpt-tui.py
@@ -1,69 +1,56 @@
 import curses
+import pexpect
+import os
 import subprocess
+import threading
 
-def draw_window(stdscr):
-    k = 0
-    cursor_x = 0
-    cursor_y = 0
+def parse_chats(chats_output):
+    lines = chats_output.decode().split('\n')
+    chats = [os.path.basename(line.strip()) for line in lines if line.strip()]
+    return chats
 
-    # Clear and refresh the screen for a blank canvas
+def start_chat_repl(chat_name):
+    child = pexpect.spawn('sgpt --repl ' + chat_name, timeout=1, encoding='utf-8')
+    return child
+
+def main(stdscr):
     stdscr.clear()
-    stdscr.refresh()
 
-    # Start colors in curses
-    curses.start_color()
-    curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)
+    chats_output = subprocess.check_output(['sgpt', '--list-chats'])
+    chats = parse_chats(chats_output)
 
-    while (k != ord('q')):
-        # Initialization
-        stdscr.clear()
-        height, width = stdscr.getmaxyx()
+    current_chat = 0
+    chat_proc = start_chat_repl(chats[current_chat])
 
-        # Declaration of strings
-        title = "ShellGPT TUI"[:width-1]
-        subtitle = "Written by Assistant"[:width-1]
-        keystr = "Last key pressed: {}".format(k)[:width-1]
-        statusbarstr = "Press 'q' to exit | STATUS BAR | Press 'r' to run ShellGPT REPL"
+    # Separate loop to read the output of the sgpt REPL
+    def read_chat_output():
+        while True:
+            try:
+                chat_proc.expect('\n')
+                line = chat_proc.before
+                if line is not None:
+                    stdscr.addstr(line + '\n')
+            except pexpect.TIMEOUT:
+                stdscr.addstr("\n> ")
+                stdscr.refresh()
+                continue
 
-        if k == 0:
-            keystr = keystr + " No key press detected..."
+    # Start the read_chat_output function in a separate thread
+    threading.Thread(target=read_chat_output).start()
 
-        # Centering calculations
-        start_x_title = int((width // 2) - (len(title) // 2) - len(title) % 2)
-        start_x_subtitle = int((width // 2) - (len(subtitle) // 2) - len(subtitle) % 2)
-        start_x_keystr = int((width // 2) - (len(keystr) // 2) - len(keystr) % 2)
+    while True:
+        for i, chat in enumerate(chats):
+            stdscr.addstr(i, 0, chat)
 
-        start_y = int((height // 2) - 2)
+        key = stdscr.getch()
+        if 32 <= key <= 126:
+            chat_proc.send(chr(key))
+        elif key == curses.KEY_UP and current_chat > 0:
+            current_chat -= 1
+            chat_proc = start_chat_repl(chats[current_chat])
+        elif key == curses.KEY_DOWN and current_chat < len(chats) - 1:
+            current_chat += 1
+            chat_proc = start_chat_repl(chats[current_chat])
 
-        # Rendering some text
-        stdscr.attron(curses.color_pair(1))
-        stdscr.attron(curses.A_BOLD)
-        stdscr.addstr(start_y - 3, start_x_title, title)
-        stdscr.addstr(start_y - 2, start_x_subtitle, subtitle)
-        stdscr.addstr(start_y, start_x_keystr, keystr)
-        stdscr.attroff(curses.color_pair(1))
-        stdscr.attroff(curses.A_BOLD)
-
-        # Render status bar
-        stdscr.attron(curses.color_pair(3))
-        stdscr.addstr(height - 1, 0, statusbarstr)
-        stdscr.addstr(height - 1, len(statusbarstr), " " * (width - len(statusbarstr) - 1))
-        stdscr.attroff(curses.color_pair(3))
-
-        # Refresh the screen
         stdscr.refresh()
-
-        # Wait for next input
-        k = stdscr.getch()
-
-        # Run shell-gpt REPL
-        if k == ord('r'):
-            command = "--repl"
-            output = subprocess.check_output(["sgpt", command])
-            stdscr.addstr(start_y + 2, 0, output)
-
-def main():
-    curses.wrapper(draw_window)
-
-if __name__ == "__main__":
-    main()
+curses.wrapper(main)
