5e276863d96ee8786a0a000e1125169610d4b066
kensleDev <kensledev@gmail.com>
2023-11-29 21:52:50 +0000

    Updated coc

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -62,10 +62,19 @@ return {
     config = function()
       require('lualine').setup({
 
+        -- sections = {
+        --   lualine_a = {'mode'},
+        --   lualine_b = {'branch', 'diff', 'diagnostics'},
+        --   lualine_c = {'filename', path = 1},
+        --   lualine_x = {'encoding', 'fileformat', 'filetype'},
+        --   lualine_y = {'progress'},
+        --   lualine_z = {'location'}
+        -- },
+       
         tabline = {
           lualine_a = { 'branch' },
           lualine_b = { 'buffers' },
-          lualine_c = { '' },
+          lualine_c = {},
           lualine_x = {},
           lualine_y = {},
           lualine_z = { 'tabs' }
