0fbb533f709cc026d4859551d851f9ee62304157
kensleDev <kensledev@gmail.com>
2023-11-29 16:18:33 +0000

    Updated plugins

--- nvim/.config/nvim/lua/config/theme.lua
+++ nvim/.config/nvim/lua/config/theme.lua
@@ -1,5 +1,5 @@
 -- Themery block
 -- This block will be replaced by Themery.
-vim.cmd("colorscheme carbonfox")
-vim.g.theme_id = 2
+vim.cmd("colorscheme duskfox")
+vim.g.theme_id = 4
 -- end themery block
--- nvim/.config/nvim/lua/plugins/navigation.lua
+++
@@ -1,7 +0,0 @@
-return {
-  { 
-    "christoomey/vim-tmux-navigator",
-    config = function() 
-    end
-  }
-}
