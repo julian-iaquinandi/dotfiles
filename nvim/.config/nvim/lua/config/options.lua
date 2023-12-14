1a05c22318183122659b9f5c2f80d086c01b4ac8
kensleDev <kensledev@gmail.com>
2023-12-14 23:13:14 +0000

    Updated tmux config

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -12,7 +12,7 @@ vim.opt.fillchars = {
 	eob = " ",
 }
 
-vim.opt.colorcolumn = "100"
+vim.opt.colorcolumn = "80"
 
 vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
