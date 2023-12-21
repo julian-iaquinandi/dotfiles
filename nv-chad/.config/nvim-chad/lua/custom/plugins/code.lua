76cf3a11e6075b6aee2c74d3a9b39804a1fcbec2
kensleDev <kensledev@gmail.com>
2023-12-21 17:31:07 +0000

    Added better ts errors

--- nv-chad/.config/nvim-chad/lua/custom/plugins/code.lua
+++ nv-chad/.config/nvim-chad/lua/custom/plugins/code.lua
@@ -1,68 +1,93 @@
 return {
-  -- lsp + formatting + linting
-  {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "rust-analyzer",
+	-- lsp + formatting + linting
+	{
+		"williamboman/mason.nvim",
+		opts = {
+			ensure_installed = {
+				"rust-analyzer",
 
-        "lua-language-server",
-        "stylua",
+				"lua-language-server",
+				"stylua",
 
-        "typescript-language-server",
-        "eslint-lsp",
-        "prettierd",
-        "svelte-language-server",
+				"typescript-language-server",
+				"eslint-lsp",
+				"prettierd",
+				"svelte-language-server",
 
-        "black",
-        "debugpy",
-        "mypy",
-        "ruff",
-        "pyright",
+				"black",
+				"debugpy",
+				"mypy",
+				"ruff",
+				"pyright",
 
-        "marksman",
+				"marksman",
 
-        "omnisharp",
-        "csharpier",
+				"omnisharp",
+				"csharpier",
 
-        "xmlformatter",
+				"xmlformatter",
 
-        "yamlfix",
+				"yamlfix",
 
-        "emmet-ls"
-      },
-    },
-  },
+				"emmet-ls",
+			},
+		},
+	},
 
-  {
-    "nvimtools/none-ls.nvim",
-    event = "VeryLazy",
-    opts = function()
-      return require "custom.configs.null-ls"
-    end,
-  },
+	{
+		"nvimtools/none-ls.nvim",
+		event = "VeryLazy",
+		opts = function()
+			return require("custom.configs.null-ls")
+		end,
+	},
 
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "plugins.configs.lspconfig"
-      require "custom.configs.lspconfig"
-    end,
-  },
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			require("plugins.configs.lspconfig")
+			require("custom.configs.lspconfig")
+		end,
+	},
 
-  {
-    "hrsh7th/nvim-cmp",
-    opts = function()
-      local M = require "plugins.configs.cmp"
-      local cmp = require "cmp"
-      M.completion.completeopt = "menu,menuone,noselect"
-      M.mapping["<CR>"] = cmp.mapping.confirm {
-        behavior = cmp.ConfirmBehavior.Insert,
-        select = false,
-      }
-      table.insert(M.sources, {name = "crates"})
-      return M
-    end,
-  },
+	{
+		"hrsh7th/nvim-cmp",
+		opts = function()
+			local M = require("plugins.configs.cmp")
+			local cmp = require("cmp")
+			M.completion.completeopt = "menu,menuone,noselect"
+			M.mapping["<CR>"] = cmp.mapping.confirm({
+				behavior = cmp.ConfirmBehavior.Insert,
+				select = false,
+			})
+			table.insert(M.sources, { name = "crates" })
+			return M
+		end,
+	},
 
+	{
+		"piersolenski/wtf.nvim",
+		dependencies = {
+			"MunifTanjim/nui.nvim",
+		},
+		opts = {},
+		keys = {
+			{
+				"gw",
+				mode = { "n", "x" },
+				function()
+					require("wtf").ai()
+				end,
+				desc = "Debug diagnostic with AI",
+			},
+			{
+				mode = { "n" },
+				"gW",
+				function()
+					require("wtf").search()
+				end,
+				desc = "Search diagnostic with Google",
+			},
+		},
+	},
 }
