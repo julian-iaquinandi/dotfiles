4f444775997ed46f5aeda6daf05030979c398d44
kensleDev <julianiaquinandi@gmail.com>
2024-02-29 18:35:24 +0000

    Update neovim keymappings

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,13 +1,30 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+local opts = { noremap = true, silent = true }
 
-vim.api.nvim_set_keymap("t", "<Esc>", "<C-\\><C-n>", { noremap = true, silent = true })
+-- Move focus
+vim.api.nvim_set_keymap("n", "<A-m>", "<C-w>h", opts)
+vim.api.nvim_set_keymap("n", "<A-,>", "<C-w>j", opts)
+vim.api.nvim_set_keymap("n", "<A-.>", "<C-w>k", opts)
+vim.api.nvim_set_keymap("n", "<A-/>", "<C-w>l", opts)
 
-vim.api.nvim_set_keymap("n", "<A-m>", "<C-w>h", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<A-,>", "<C-w>j", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<A-.>", "<C-w>k", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<A-/>", "<C-w>l", { noremap = true, silent = true })
+-- Exit insert mode
+vim.api.nvim_set_keymap("i", "jk", "<ESC><C-w>w i", opts)
+vim.api.nvim_set_keymap("i", "jj", "<ESC>", opts)
+vim.api.nvim_set_keymap("i", "jl", "<C-\\><C-n><C-w>w :lua SwapBufferAndResize()<CR>", { noremap = true })
+
+-- Exit terminal mode
+vim.api.nvim_set_keymap("t", "jk", "<C-\\><C-n><C-w>w", { noremap = true })
+vim.api.nvim_set_keymap("t", "jj", "<C-\\><C-n>", { noremap = true })
+vim.api.nvim_set_keymap("t", "jl", "<C-\\><C-n><C-w>w :lua SwapBufferAndResize()<CR>", { noremap = true })
+
+-- Remap movement keys
+vim.api.nvim_set_keymap("n", "j", "h", opts)
+vim.api.nvim_set_keymap("n", "k", "j", opts)
+vim.api.nvim_set_keymap("n", "l", "k", opts)
+vim.api.nvim_set_keymap("n", ";", "l", opts)
 
 -- return {
 --   { "<A-m>", "<cmd><C-w>h", "focus left" },
--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -2,13 +2,18 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- function OS()
---   return package.config:sub(1, 1) == "\\" and "win" or "unix"
--- end
+local powershell_options = {
+  shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell",
+  shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+  shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
+  shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
+  shellquote = "",
+  shellxquote = "",
+}
 
--- if OS() == "win" then
-vim.o.shell = "C:/Windows/System32/WindowsPowerShell/v1.0/powershell.exe"
--- end
+for option, value in pairs(powershell_options) do
+  vim.opt[option] = value
+end
 
 if vim.g.neovide then
   vim.o.guifont = "JetBrainsMono Nerd Font:h11"
